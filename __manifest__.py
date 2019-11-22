f10338efe27726837f27451f4feaae3d11f01938
cesargtz <cesareomargtz@gmail.com>
2019-11-22 15:38:26 -0700

    add report

--- __manifest__.py
+++ __manifest__.py
@@ -25,6 +25,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
+        'views/report_quotation.xml'
         # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
